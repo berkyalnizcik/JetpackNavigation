4e1ebed1acf989763c1ae0b8446ad2221cfb23d8
berk yalnızcık <byalnzck@gmail.com>
2021-12-30 04:45:00 +0300

    navigation version update + deeplink added

---
+++ app/src/main/java/com/berk/jetpacknavigation/fragment/deeplink/DeepLinkAppWidgetProvider.kt
@@ -0,0 +1,33 @@
+package com.berk.jetpacknavigation.fragment.deeplink
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.os.Bundle
+import android.widget.RemoteViews
+import androidx.navigation.NavDeepLinkBuilder
+import com.berk.jetpacknavigation.R
+
+class DeepLinkAppWidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        val remoteViews = RemoteViews(
+            context.packageName,
+            R.layout.deep_link_appwidget
+        )
+
+        val args = Bundle()
+        args.putString("myarg", "From Widget")
+        val pendingIntent = NavDeepLinkBuilder(context)
+            .setGraph(R.navigation.nav_graph)
+            .setDestination(R.id.deeplink_dest)
+            .setArguments(args)
+            .createPendingIntent()
+
+        remoteViews.setOnClickPendingIntent(R.id.deep_link_button, pendingIntent)
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews)
+    }
+}
